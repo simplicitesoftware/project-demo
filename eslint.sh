eea3cdb6ebf28b364299a5a604dd874edef43622
David AZOULAY <dazoulay@simplicite.fr>
2024-12-13 11:32:56 +0100

    Added eslint.sh

---
+++ eslint.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+for f in Demo*
+do
+	if [ -d $f ]
+	then
+		pushd $f > /dev/null
+		echo
+		echo "========================="
+		echo $f
+		echo "========================="
+		npm i
+		npm run eslint
+		RES=$?
+		popd > /dev/null
+		[ $RES -ne 0 ] && exit $RES
+	fi
+done
+echo
+exit 0
