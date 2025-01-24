61727dfb25aa7f4c9cb9cd67a9ad8f9dba705164
David AZOULAY <dazoulay@simplicite.fr>
2025-01-24 16:34:02 +0100

    Added custom disp module

--- clone.sh
+++ clone.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-[ ! -d Demo ] &&     git clone --depth 1 --single-branch --branch=v6 git@github.com:simplicitesoftware/module-demo.git      Demo
-[ ! -d DemoAPIs ] && git clone --depth 1 --single-branch --branch=v6 git@github.com:simplicitesoftware/module-demo-apis.git DemoAPIs
+[ ! -d Demo ] &&           git clone --depth 1 --single-branch --branch=v6 git@github.com:simplicitesoftware/module-demo.git                    Demo
+[ ! -d DemoAPIs ] &&       git clone --depth 1 --single-branch --branch=v6 git@github.com:simplicitesoftware/module-demo-apis.git               DemoAPIs
+[ ! -d DemoCustomDisp ] && git clone --depth 1 --single-branch --branch=v6 git@github.com:simplicitesoftware/module-demo-custom-disposition.git DemoCustomDisp
 
 exit 0
