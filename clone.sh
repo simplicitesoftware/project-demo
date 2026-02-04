51ce300739b8f18ee6803518bef55da46576451a
David AZOULAY <dazoulay@simplicite.fr>
2026-02-04 18:06:18 +0100

    Added DemoDashboard v6

--- clone.sh
+++ clone.sh
@@ -3,5 +3,6 @@
 [ ! -d Demo ] &&           git clone --depth 1 --single-branch --branch=v6 git@github.com:simplicitesoftware/module-demo.git                    Demo
 [ ! -d DemoAPIs ] &&       git clone --depth 1 --single-branch --branch=v6 git@github.com:simplicitesoftware/module-demo-apis.git               DemoAPIs
 [ ! -d DemoCustomDisp ] && git clone --depth 1 --single-branch --branch=v6 git@github.com:simplicitesoftware/module-demo-custom-disposition.git DemoCustomDisp
+[ ! -d DemoDashboard ] &&  git clone --depth 1 --single-branch --branch=v6 git@github.com:simplicitesoftware/module-demo-dashboard.git          DemoDashboard
 
 exit 0
