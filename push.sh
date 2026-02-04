80d903d16937cadf0d4143e6b343212a2d76e2b1
David AZOULAY <dazoulay@simplicite.fr>
2026-02-04 14:06:14 +0100

    Tag

--- push.sh
+++ push.sh
@@ -9,6 +9,7 @@ do
 		echo "========================="
 		echo $f
 		echo "========================="
+		git tag -f 5 -m "Version 6" && git push -f ${1:-github} 6
 		git push ${1:-github}
 		popd > /dev/null
 	fi
