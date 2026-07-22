35118d8a61cd9ab94d25b8d1ac7081e1ce2d0a81
David AZOULAY <dazoulay@simplicite.fr>
2026-07-22 17:41:29 +0200

    Fixed tag

--- push.sh
+++ push.sh
@@ -9,7 +9,7 @@ do
 		echo "========================="
 		echo $f
 		echo "========================="
-		git tag -f 5 -m "Version 6" && git push -f ${1:-github} 6
+		git tag -f 5 -m "Version 5" && git push -f ${1:-github} 5
 		git push ${1:-github}
 		popd > /dev/null
 	fi
